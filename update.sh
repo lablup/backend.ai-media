fe96cef0d70f1416138a8405f4f29dd1aecc1377
Joongi Kim <me@daybreaker.info>
2016-07-15 11:50:42 +0900

    refs #2: Show output when running update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-hash=$(webpack | grep 'Hash: ' | awk '{ print $2; }')
+raw_hash=$(webpack --color | tee >(cat 1>&2) | grep 'Hash: ' | awk '{ print $2; }')
+hash=$(echo $raw_hash | perl -pe 's/\e\[?.*?[\@-~]//g')
 cd assets
 for name in $(ls | grep '[a-f0-9]\{20\}' | grep -v $hash); do
   rm -r $name
